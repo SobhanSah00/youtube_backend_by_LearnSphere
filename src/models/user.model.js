5a513d146ba6e701fa01c3b5e9dd4352db39fbe0
sobhan9012 <sobhansahoo2000@gmail.com>
2024-04-26 16:45:11 +0530

    adding some methods in user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -52,4 +52,45 @@ const userSchema = new Schema(
     }
 )
 
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) return next()
+    // hash password
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccesstoken = function() {
+    jwt.sign(
+        {
+            _id: this._id,
+            email : this.email,
+            username : this.username,
+            fullName : this.fullName,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken= function() {
+    jwt.sign(
+        {
+            _id: this._id,
+            email : this.email,
+            username : this.username,
+            fullName : this.fullName,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
 export const User = mongoose.model("User", userSchema)
