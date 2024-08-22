ae732c6dd896730e4b0d0cbb3905994c565f8669
sobhan9012 <sobhansahoo2000@gmail.com>
2024-08-22 08:46:22 +0530

    "Refactored app.js, added like routes and controllers, and updated imports and exports"

--- src/app.js
+++ src/app.js
@@ -1,32 +1,33 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-const app = express()
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app = express();
 
-app.use(cors(
-    {
-        origin: process.env.CORS_ORIGIN,
-        credentials: true
-    }
-)) 
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-app.use(express.json({limit:"50mb"}))
-app.use(express.urlencoded({extended:true,limit:"50mb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
- 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 //routes imports
 
-import userRouter from "./routes/user.routes.js"
+import userRouter from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js";
-import tweetRouter from "./routes/tweet.routes.js"
+import tweetRouter from "./routes/tweet.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import commentRouter from "./routes/comment.routes.js";
 
 //routes decalration
-app.use("/api/v1/users",userRouter)
-app.use("/api/v1/video",videoRouter)
-app.use("/api/v1/tweet",tweetRouter)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/video", videoRouter);
+app.use("/api/v1/tweet", tweetRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/comments", commentRouter);
 
-export {app}
-
-
+export { app };
