510512876366056a54c3dc02b02c65efb9e32989
sobhan9012 <sobhansahoo2000@gmail.com>
2024-08-27 02:38:05 +0530

    adding the subscription controller

---
+++ src/controllers/subscription.controller.js
@@ -0,0 +1,65 @@
+import mongoose, {isValidObjectId} from "mongoose"
+import {User} from "../models/user.model.js"
+import { Subscription } from "../models/subscription.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandeler.js"
+
+// TODO: toggle subscription   
+const toggleSubscription = asyncHandler(async (req, res) => {
+    const {channelId} = req.params
+
+    if(!isValidObjectId(channelId)) {
+        throw new ApiError(400,"Invalid channelId")
+    }
+
+    const isSubscribed = await Subscription.findOne({
+        subscriber: req.user?._id,
+        channel: channelId,
+    })
+
+    if(isSubscribed) {
+        await Subscription.findByIdAndDelete(isSubscribed?._id)
+
+        return res
+            .status(200)
+            .json(new ApiResponse(
+                200,
+                {
+                    subscribed : false
+                },
+                "unsubscribed successfully"
+            ))
+    }
+
+    await Subscription.create({
+        subscriber : req.user?._id,
+        channel : channelId
+    })
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                { subscribed: true },
+                "subscribed successfully"
+            )
+        );   
+})
+
+// TODO: controller to return subscriber list of a channel
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+    const {channelId} = req.params
+})
+
+// controller to return channel list to which user has subscribed
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+    const { subscriberId } = req.params
+})
+
+export {
+    toggleSubscription,
+    getUserChannelSubscribers,
+    getSubscribedChannels
+}
